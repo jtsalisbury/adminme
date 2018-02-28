8f1d314a4f4ec0cf61b24cf7be569ba1f84c1ef7
crazyscouter <crazyscouter1@gmail.com>
2018-02-28 15:57:52 -0500

    Add command to display play time

---
+++ lua/adminme_commands/time.lua
@@ -0,0 +1,14 @@
+am.addCMD("mytime", "Retrieves and shows your play time", "General", function(ply)
+	local q = am.db:select("play_times"):where("steamid", ply:SteamID()):callback(
+		function(res)
+			if (!res) then 
+				am.notify(ply, "Error retrieving data!")
+				return
+			end
+			
+			local timePretty = string.FormattedTime(res[1]["play_time_seconds"])
+
+			am.notify(ply, "You have played ", am.green, timePretty["h"], am.def, " hours, ", am.green, timePretty["m"], am.def, " minutes, and ", am.green, timePretty["s"], am.def, " seconds!")
+		end)
+	:execute()
+end)
